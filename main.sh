a711089499d4e8d3d2069e470d15f1f5ec166f89
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2021-01-26 19:36:50 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env /usr/bin/bash
 
-#Copyright (C) 2020  Alan Burr
+#Copyright (C) 2020-2021  Alan Burr
 #
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
